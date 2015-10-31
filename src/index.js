b97c8bf5de75e121112caf62ded4f4b077f650cf
Alex Puschinsky <alexpoo@gmail.com>
2015-10-31 19:09:34 +0200

    Create main page
    Use CodeMirror as code editor

---
+++ src/index.js
@@ -0,0 +1,48 @@
+require('./style.sass')
+require('../node_modules/codemirror/lib/codemirror.css')
+require('../node_modules/codemirror/theme/elegant.css')
+
+import CodeMirror from '../node_modules/codemirror/lib/codemirror.js'
+import {} from '../node_modules/codemirror/mode/javascript/javascript.js'
+
+import whynomatch from 'whynomatch'
+import $ from 'jquery'
+
+
+$(function(){
+  let objectEditor = createEditor('.object');
+  let queryEditor = createEditor('.query');
+  let outputEditor = createEditor('.output', {readonly: true});
+
+  objectEditor.on('change', function(){
+    update(objectEditor, queryEditor, outputEditor);
+  })
+
+  queryEditor.on('change', function(){
+    update(objectEditor, queryEditor, outputEditor);
+  })
+})
+
+function createEditor(selector, options = {}){
+  let element = $(selector)[0];
+
+  return CodeMirror(element, { 
+    theme: 'elegant',
+    lineNumbers: true,
+    matchBrackets: true,
+    readOnly: options.readonly || false,
+    mode: {
+      name: 'javascript',
+      json: true
+    }
+  }); 
+}
+
+function update(objectEditor, queryEditor, outputEditor){
+  let object = JSON.parse(objectEditor.getValue());
+  let query = JSON.parse(queryEditor.getValue());
+  if(object && query){
+    let result = whynomatch(object, query);
+    outputEditor.setValue(JSON.stringify(result, null, 2));
+  }
+}

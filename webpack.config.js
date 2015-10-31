3cb1927fec261df834205a19983aa4a4f4c31854
Alex Puschinsky <alexpoo@gmail.com>
2015-10-31 19:04:21 +0200

    Configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var WebpackNotifierPlugin = require('webpack-notifier');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var path = require("path");
 module.exports = {
   entry: {
@@ -14,10 +15,25 @@ module.exports = {
         test   : /.js$/,
         loader : 'babel-loader',
         exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader!autoprefixer-loader")
+      },
+      {
+        test: /\.sass$/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader!autoprefixer-loader!sass-loader")
       }
     ]
   },
+  sassLoader: {
+    indentedSyntax: true
+  },
   plugins: [
     new WebpackNotifierPlugin(),
-  ]
+    new ExtractTextPlugin("index.css")
+  ],
+  externals: {
+    "jquery": "jQuery"
+  }
 }

a4c9ca7b8721e1ddec2aed522adf7f192b1dd4f0
Alex Puschinsky <alexpoo@gmail.com>
2015-11-06 19:03:52 +0200

    Fix MongoParsingError

--- src/mongo_parsing_error.js
+++ src/mongo_parsing_error.js
@@ -1,6 +1,10 @@
 class MongoParsingError extends Error{
-  constructor(operator, expected, value){
-    super(`${key} operator must recieve a ${expected}. Got: ${recived}`);
+  constructor(operator, expected, recived){
+    let message = `${operator} operator must recieve a ${expected}. Got: ${recived}`;
+    super(message);
+    this.name = this.constructor.name;
+    this.message = message; 
+    Error.captureStackTrace(this, this.constructor.name);
   }
 }
 

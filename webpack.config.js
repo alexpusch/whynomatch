fc381e7d9178b1516379b4912cf9af71cf7b1ed8
Alex Puschinsky <alexpoo@gmail.com>
2015-11-06 22:57:08 +0200

    Reconfigure project for UMD

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,31 @@
 var WebpackNotifierPlugin = require('webpack-notifier');
 
 module.exports = {
-  entry: "./index.js",
+  target: "node",
+  entry: "./src/whynomatch.js",
   output: {
       path: "dist",
-      filename: "bundle.js"
+      filename: "whynomatch.js",
+      library: "whynomatch",
+      libraryTarget: "umd"
   },
   module : {
     loaders: [ { 
         test   : /.js$/,
-        loader : 'babel-loader?optional[]=runtime',
+        loader : 'babel-loader',
         exclude: /node_modules/
       }
     ]
   },
   plugins: [
     new WebpackNotifierPlugin(),
-  ]
+  ],
+  externals: {
+    'lodash': {
+      root: '_',
+      commonjs2: 'lodash',
+      commonjs: 'lodash',
+      amd: 'lodash'
+    }
+  }
 }

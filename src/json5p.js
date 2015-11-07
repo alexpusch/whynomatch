ee1c584c4d19ae76b92b9833089934aec17111ce
Alex Puschinsky <alexpoo@gmail.com>
2015-11-07 19:08:56 +0200

    Add complex type support
    Regex - /.../
    ObjectID(...)
    ISODate(...)

--- src/json5p.js
+++ src/json5p.js
@@ -2,14 +2,26 @@ import JSON5 from "json5"
 
 let regexRegex = /(\/(.+?)\/([gimy]*))/g
 
+let objectIdRegex = /ObjectId\([\'\"](\w{24})[\'\"]\)/;
+
+let isoDateRegexBase = "(\\d{4})-(\\d{2})-(\\d{2})T(\\d{2}):(\\d{2}):(\\d{2}(?:\\.\\d*)?)(?:Z|(\\+|-)([\\d|:]*))?"
+let isoDateRegex = new RegExp(`ISODate\\([\\'\\"](${isoDateRegexBase})[\\'\\"]\\)`)
+
 function parse(str){
-  str = _endcodeRegex(str);
+  str = _encode(str);
   let parsedStr = JSON5.parse(str, _decodeRegex);
   return parsedStr;
 }
 
 function stringify(obj){
   return JSON5.stringify(obj, _stringifyRegex, 2);
+}
+
+function _encode(value){
+  value = _endcodeRegex(value);
+  value = _encodeObjectId(value);
+  value = _encodeIsoDate(value);
+  return value;
 }
 
 function _endcodeRegex(value){
@@ -28,6 +40,14 @@ function _decodeRegex(key, value){
   } else{
     return value;
   }
+}
+
+function _encodeObjectId(value){
+  return value.replace(objectIdRegex, "\"ObjectId(\'$1\')\""); 
+}
+
+function _encodeIsoDate(value){
+  return value.replace(isoDateRegex, "\"ISODate(\'$1\')\""); 
 }
 
 function _stringifyRegex(key, value){
